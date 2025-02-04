a06bbd9fe68a767017635afdd67a2428d4ca32ba
coiilee <dakyung96@gmail.com>
2025-02-04 15:46:47 +0900

    kakao API Login/signUp

---
+++ src/main/java/kh/academy/Pizza/controller/SocialViewController.java
@@ -0,0 +1,23 @@
+package kh.academy.Pizza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SocialViewController {
+
+    //카카오 로그인 html 연결
+    //indexViewController 에서 index.html 내 카카오 네이버 로그인 버튼 설정
+    //카카오 회원가입 html 연결
+    @GetMapping("/signup/kakao")
+    public String kakaoSignup(){
+        return "signup/kakao";
+    }
+
+    //네이버 회원가입 html 연결
+    @GetMapping("/signup/naver")
+    public String naverSignup(){
+        return "signup/naver";
+    }
+
+}
