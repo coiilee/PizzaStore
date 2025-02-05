958bffc883ccbcd4a69ce2896f484e150481df24
coiilee <dakyung96@gmail.com>
2025-02-05 12:46:18 +0900

    kakaomap

--- src/main/java/kh/academy/Pizza/controller/indexViewController.java
+++ src/main/java/kh/academy/Pizza/controller/indexViewController.java
@@ -1,14 +1,26 @@
 package kh.academy.Pizza.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexViewController {
+
+    @Value("${kakao.javacript.app.key}")
+    private String kakaoJSAppKey;
+
     @GetMapping("/")
     public String index() {
         //index.html -> 회원가입 / 로그인 버튼 클릭해서 이동 설정
         //피자 메뉴 목록 모두 조회 버튼 클릭해서 이동 설정
         return "index";
+    }
+
+    @GetMapping("/kakao/map")
+    public String map(Model model) {
+        model.addAttribute("appkey", kakaoJSAppKey);
+        return "kakaoMap";
     }
 }
